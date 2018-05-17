12ba61564f43e0fd8451af0fe0026e5c8c19de2e
Rhian Castro <rhi.castro@gmail.com>
2018-05-16 22:34:03 -0300

    3.5 chapter 3.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'youreweaksasuskeyoulackofhashes'
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Orcola'}
+    return render_template('index.html', title='Home', user=user)
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
